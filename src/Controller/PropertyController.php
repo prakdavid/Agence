420a7ec44aa75e6b2b6e57ad8234cb21b838214c
prak <prak.david@gmail.com>
2019-11-14 18:12:46 +0100

    init

---
+++ src/Controller/PropertyController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class PropertyController extends AbstractController
+{
+    /**
+     * @Route("/property", name="property.index")
+     */
+    public function index()
+    {
+
+
+        return $this->render('property/index.html.twig', [
+            'controller_name' => 'PropertyController',
+        ]);
+    }
+}
